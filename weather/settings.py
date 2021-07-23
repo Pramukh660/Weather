30aa089e930d99d90b531f253a77667aff988c47
pramukh660 <pramukh660@gmail.com>
2021-07-24 00:35:12 +0530

    true

--- weather/settings.py
+++ weather/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$wa79*1k^!=^mfhe5penx!1hch2p-$=7tag8s*w2ek^i-*f&br'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['waether-api.herokuapp.com']
 
