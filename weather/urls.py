75dd299d270a2a10e168d5ecb082eb226a3feb60
pramukhjs <pramukh660@gmail.com>
2021-08-01 17:04:08 +0530

    static in settings

--- weather/urls.py
+++ weather/urls.py
@@ -19,7 +19,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
