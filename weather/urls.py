aed58afd9df0d668f868eaf53fed109ae2119db4
pramukhjs <pramukh660@gmail.com>
2021-08-01 20:31:55 +0530

    urls

--- weather/urls.py
+++ weather/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.views import generic
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    # path('', views.index, name='index'),
+    path('', generic.TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
