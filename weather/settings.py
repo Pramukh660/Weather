ae54a2e160481bf05022b7139a109f04819a660d
pramukhjs <pramukh660@gmail.com>
2021-08-01 20:46:16 +0530

    requirements

--- weather/settings.py
+++ weather/settings.py
@@ -139,6 +139,8 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
