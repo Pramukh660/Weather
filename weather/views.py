6e51320ff61806a73b3d79229b964420ebe6b24a
pramukhjs <pramukh660@gmail.com>
2021-08-04 14:03:22 +0530

    added another field

--- weather/views.py
+++ weather/views.py
@@ -11,8 +11,6 @@ def index2(request):
 
 
 def index(request):
-    if request.method != 'POST':
-       return render(request, "index.html")
     if request.method == 'POST':
         city = request.POST['city']
         source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +
@@ -22,7 +20,7 @@ def index(request):
             "country_code": str(list_of_data['sys']['country']),
             "coordinate": str(list_of_data['coord']['lon']) + ', '+ str(list_of_data['coord']['lat']),
             "temp": str(list_of_data['main']['temp']) + ' °C',
-            'tmp_felling': str(list_of_data['main']['feels_like']) + ' °C',
+            "tmp_felling": str(list_of_data['main']['feels_like']) + ' °C',
             "pressure": str(list_of_data['main']['pressure']),
             "humidity": str(list_of_data['main']['humidity']),
             'main': str(list_of_data['weather'][0]['main']),
@@ -32,7 +30,3 @@ def index(request):
     else:
         data = {}
     return render(request, "index.html", data)
-
-source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&units=metric&appid=33e29300b01a9e8984fd89c44a5b99ad').read()
-list_of_data = json.loads(source)
-print(list_of_data)
