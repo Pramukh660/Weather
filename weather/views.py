2074db7921451cd6b3f6b1a39deac870809a3769
pramukhjs <pramukh660@gmail.com>
2021-08-04 13:48:35 +0530

    temp feeling

--- weather/views.py
+++ weather/views.py
@@ -13,16 +13,14 @@ def index2(request):
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        if city == 'banglore' or city == 'Banglore' or city == 'bangalore' or city == 'Bangalore':
-            city = 'bengaluru'
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +
                     city + '&units=metric&appid=33e29300b01a9e8984fd89c44a5b99ad').read()
         list_of_data = json.loads(source)
         data = {
             "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ', '
-            + str(list_of_data['coord']['lat']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '+ str(list_of_data['coord']['lat']),
             "temp": str(list_of_data['main']['temp']) + ' °C',
+            'tmp_felling': str(list_of_data['main']['feels_like']) + ' °C',
             "pressure": str(list_of_data['main']['pressure']),
             "humidity": str(list_of_data['main']['humidity']),
             'main': str(list_of_data['weather'][0]['main']),
@@ -32,3 +30,7 @@ def index(request):
     else:
         data = {}
     return render(request, "index.html", data)
+
+source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&units=metric&appid=33e29300b01a9e8984fd89c44a5b99ad').read()
+list_of_data = json.loads(source)
+print(list_of_data)
